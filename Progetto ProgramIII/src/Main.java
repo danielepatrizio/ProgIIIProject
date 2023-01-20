096e84c29de32dc53059e368bd644a3106f344d3
Diego <diego.gelsi001@studenti.uniparthenope.it>
2023-01-20 18:19:04 +0100

    update v3.2 aggiornamento lettura file

--- Progetto ProgramIII/src/Main.java
+++ Progetto ProgramIII/src/Main.java
@@ -2,8 +2,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.Reader;
 import java.util.ArrayList;
-
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Main{
 
 
@@ -21,8 +22,34 @@ public class Main{
     //lettura file
 public void GetFileInfo(){
 
-    File myObj = new File("filename.txt");
+
+
+        //Path della posizione su disco del file di testo che vogliamo leggere in input
+        String pathFileName= "./folder/inputFile.txt";
+
+        File inputFile = new File(pathFileName);
+
+        //Creo un oggetto Scanner con fonte di dati il file inputFile.txt
+        //che si trova all'interno della cartella folder, all'interno del mio progetto Java
+        Scanner scannerDaFile = null;
+        try {
+            scannerDaFile = new Scanner(inputFile);
+
+            System.out.println("------------------ STAMPA DEL FILE DI TESTO: "+inputFile.getName()+" ----------------------");
+
+            while(scannerDaFile.hasNextLine()) {
+                System.out.println(scannerDaFile.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }finally {
+            if(scannerDaFile!=null) {
+                scannerDaFile.close();
+            }
+        }
+
      }
     }
+
 
 
