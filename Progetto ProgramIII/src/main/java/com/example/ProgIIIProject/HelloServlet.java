479a122e338183f2f07eae4518c77ae4ceebda93
DanyPtz <113537213+DanyPtz@users.noreply.github.com>
2023-02-10 20:20:39 +0100

    Final changements 2.0

--- Progetto ProgramIII/src/main/java/com/example/ProgIIIProject/HelloServlet.java
+++ Progetto ProgramIII/src/main/java/com/example/ProgIIIProject/HelloServlet.java
@@ -8,7 +8,21 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 
+
+import java.io.IOException;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -29,10 +43,15 @@ public String customeWelcome(@PathParam("name")String name){
         response.setContentType("text/html");
 
         // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        ServletOutputStream out = response.getOutputStream();
+        out.println("<html>");
+        out.println("<head><title>Router Web Application</title></head>");
+        out.println("<body>");
+        out.println("<h1>Router Web Application</h1>");
+        out.println("<p>Welcome to the Router Web Application.</p>");
+        out.println("<p>You can manage your network routes here.</p>");
+        out.println("</body>");
+        out.println("</html>");
     }
 
     public void destroy() {
