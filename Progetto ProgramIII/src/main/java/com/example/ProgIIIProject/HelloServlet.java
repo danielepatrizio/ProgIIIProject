fdc886aa7b756b5df4c5399f7c03aaf9791be1d0
DanyPtz <113537213+DanyPtz@users.noreply.github.com>
2023-02-10 20:31:32 +0100

    Final changements 3.0

--- Progetto ProgramIII/src/main/java/com/example/ProgIIIProject/HelloServlet.java
+++ Progetto ProgramIII/src/main/java/com/example/ProgIIIProject/HelloServlet.java
@@ -1,5 +1,6 @@
 package main.java.com.example.ProgIIIProject;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -39,10 +40,8 @@ public class HelloServlet extends HttpServlet {
 public String customeWelcome(@PathParam("name")String name){
         return "welcome" + name + "! !!!!!";
 }
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-
-        // Hello
         ServletOutputStream out = response.getOutputStream();
         out.println("<html>");
         out.println("<head><title>Router Web Application</title></head>");
@@ -50,8 +49,24 @@ public String customeWelcome(@PathParam("name")String name){
         out.println("<h1>Router Web Application</h1>");
         out.println("<p>Welcome to the Router Web Application.</p>");
         out.println("<p>You can manage your network routes here.</p>");
+        out.println("<h2>Add a New Route</h2>");
+        out.println("<form action='/addRoute' method='post'>");
+        out.println("<label for='destination'>Destination:</label>");
+        out.println("<input type='text' id='destination' name='destination' required>");
+        out.println("<label for='mask'>Mask:</label>");
+        out.println("<input type='text' id='mask' name='mask' required>");
+        out.println("<label for='gateway'>Gateway:</label>");
+        out.println("<input type='text' id='gateway' name='gateway' required>");
+        out.println("<input type='submit' value='Add Route'>");
+        out.println("</form>");
+        out.println("<h2>Current Routes</h2>");
+        out.println("<table>");
+        out.println("<tr><th>Destination</th><th>Mask</th><th>Gateway</th></tr>");
+
+        out.println("</table>");
         out.println("</body>");
         out.println("</html>");
+
     }
 
     public void destroy() {
